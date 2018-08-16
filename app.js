8028c2346d546f9040934804e5ae93cbe8fe62a7
sagittaria <shuyang_x@hotmail.com>
2018-08-17 01:29:15 +0800

    文章保存成功.Next:重规划API

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var testRouter = require('./routes/test');
+var postRouter = require('./routes/stack/post');
 
 var app = express();
 
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/test', testRouter);
+app.use('/api', postRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
